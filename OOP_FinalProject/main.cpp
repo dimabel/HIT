faa82822d66c6fa2adcfd7f9550373c822ced044
Daniel Gorlovetsky <danielgorlovetsky@daniels-mbp.il.outbrain.com>
2013-12-08 15:06:17 +0200

    Initial Commit

---
+++ OOP_FinalProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  OOP_FinalProject
+//
+//  Created by Daniel Gorlovetsky on 12/8/13.
+//  Copyright (c) 2013 Daniel Gorlovetsky. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
